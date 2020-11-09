a638e4937fa9148466b89db5a052b94011e338c1
learneng <learneng99@gmail.com>
2020-11-09 22:16:26 +0530

    First commit

--- myApp.js
+++ myApp.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();
 
 app.get("/json", (req, res) => {
-    res.json({
-        "message": "Hello world"
-    });
+    res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+    res.json({
+        "message": "Hello json"
+    });
+})
 
 
 
