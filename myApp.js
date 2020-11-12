6809ee5260df09b464099dcdb6f3f332a6e5624b
learneng <learneng999@gmail.com>
2020-11-12 17:09:30 +0530

    Seventh challenge

--- myApp.js
+++ myApp.js
@@ -25,4 +25,13 @@ app.get("/json", (req, res) => {
     }
 });
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({
+        "time": req.time
+    });
+});
+
 module.exports = app;
